54df446c926b644a8e90afbcd7d4a987dd34c162
mangsuyo <mangsuyo3610@gmail.com>
2024-07-26 00:29:54 +0900

    feat: 백엔드에 인가코드 전달하기

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,38 @@
+import React, { useEffect } from "react";
 import "./App.css";
+import axios from "axios";
+import { useLocation } from "react-router-dom";
 
-function App() {
+export default function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const queryParams = new URLSearchParams(location.search);
+        const codeParams = queryParams.toString();
+        const url = `${
+          import.meta.env.VITE_API_BASE_URL
+        }/oauth2/google/user?${codeParams}`;
+        const response = await axios.get(url);
+
+        if (window.ReactNativeWebView) {
+          window.ReactNativeWebView.postMessage(JSON.stringify(response.data));
+        }
+      } catch (error) {
+        console.error("Error fetching user data:", error);
+
+        if (window.ReactNativeWebView) {
+          window.ReactNativeWebView.postMessage(
+            JSON.stringify({ error: error.message })
+          );
+        }
+      }
+    };
+
+    fetchUserData();
+  }, [location.search]);
+
   return (
     <>
       <div>사용 중이던 서비스로 이동하여</div>
@@ -8,5 +40,3 @@ function App() {
     </>
   );
 }
-
-export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
 import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
