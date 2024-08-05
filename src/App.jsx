6b371fea5ab14374a26dc7ac7e22b42ad252e3a9
mangsuyo <mangsuyo3610@gmail.com>
2024-08-05 20:39:57 +0900

    feat: data필드로 파람 보내기

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,30 @@
 import React, { useEffect } from "react";
 import "./App.css";
-import axios from "axios";
 import { useLocation } from "react-router-dom";
 
 export default function App() {
   const location = useLocation();
+
   useEffect(() => {
     const fetchUserData = async () => {
       try {
         const queryParams = new URLSearchParams(location.search);
         const codeParams = queryParams.toString();
         console.log(codeParams);
+
         if (window.ReactNativeWebView) {
-          window.ReactNativeWebView.postMessage(JSON.stringify(codeParams));
+          window.ReactNativeWebView.postMessage(
+            JSON.stringify({ data: codeParams })
+          );
         }
       } catch (error) {
         console.error("Error fetching user data:", error);
 
         if (window.ReactNativeWebView) {
           window.ReactNativeWebView.postMessage(
-            JSON.stringify({ error: error.message })
+            JSON.stringify({
+              error: "Error fetching user data: " + error.message,
+            })
           );
         }
       }
